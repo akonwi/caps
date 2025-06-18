569aaf7eeb30f29e51219cde0469eb8d7e826127
Akonwi Ngoh <akonwi@gmail.com>
2025-06-17 20:01:21 -0400

    :lipstick:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="add-hat" options={{ title: "Add Hat" }} />
         <Stack.Screen name="edit-hat" options={{ title: "Edit Hat" }} />
         <Stack.Screen name="+not-found" />
